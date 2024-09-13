85a6cda116cf05d5e79fa1bcb689c1ba082eb045
Lylay-09 <pozzlay549@gmail.com>
2024-09-13 14:22:49 +0700

    add tax and void col v2

--- routes/api.php
+++ routes/api.php
@@ -66,9 +66,10 @@ Route::middleware('jwt')->group(function(){
         Route::get('',[TaxController::class,'getTaxes']);
         Route::get('{id?}',[TaxController::class,'getTax']);
         Route::put('{id?}',[TaxController::class,'update']);
+        Route::delete('{id?}',[TaxController::class,'delete']);
         Route::put('void/{id?}',[TaxController::class,'void']);
         Route::put('unvoid/{id?}',[TaxController::class,'unVoide']);
-        Route::delete('{id?}',[TaxController::class,'delete']);
+
     });
 
     Route::prefix('bank')->group(function(){
