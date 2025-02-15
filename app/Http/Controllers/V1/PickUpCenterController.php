59a77b01f523a21e0a0b6cc0f513ef6c86c53537
Lylay-09 <pozzlay549@gmail.com>
2025-02-15 16:01:32 +0700

    print pkg v3

--- app/Http/Controllers/V1/PickUpCenterController.php
+++ app/Http/Controllers/V1/PickUpCenterController.php
@@ -491,7 +491,7 @@ class PickUpCenterController extends Controller
             $package->merchant_name = $package->merchant->user_name;
             $package->merchant_phone = $package->merchant->phone;
             $package->delivery_fee = $package->delivery_fee + $package->taxi_fee + $package->extra_charge;//($package->cod ? $package->price : 0);
-            $package->base_fee = $package->delivery_fee;
+            $package->base_fee = $packages->payer == 'receiver' ? $package->delivery_fee:0;
             $package->created_by = $package->updateUser->user_name;
             $package->created_date = Helper::formatCustomDateTime($package->created_at,'d-M-Y');
             $package->warehouse_at = Helper::dateDMY($package->arrive_warehouse_datetime);
