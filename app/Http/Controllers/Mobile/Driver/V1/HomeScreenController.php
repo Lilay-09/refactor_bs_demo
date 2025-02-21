eebba272bc7a1000e7e6067abe704a1cfab3178e
Lylay-09 <pozzlay549@gmail.com>
2025-02-21 16:01:44 +0700

    driver submit package not change cod v1

--- app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
+++ app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
@@ -460,7 +460,7 @@ class HomeScreenController extends Controller
         if($status_id == 19) {
             $inputs['failed_datetime'] = now();
             $inputs['failure_notes'] = $deliveryRemarks;
-            $package->price = 0;
+            // $package->price = 0;
         }
 
         if($payer){
