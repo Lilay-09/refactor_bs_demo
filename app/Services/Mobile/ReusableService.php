9ff225d588c46096f66f8542287666eb51b33c26
Lylay-09 <pozzlay549@gmail.com>
2025-03-08 13:31:12 +0700

    driver history package correct status

--- app/Services/Mobile/ReusableService.php
+++ app/Services/Mobile/ReusableService.php
@@ -44,7 +44,7 @@ class ReusableService
         ->join('users as m','m.id','p.merchant_id')
         ->leftJoin('payments as pmt','pmt.id','p.driver_payment_id')
         // ->leftJoin('payments as pmt','pmt.id','p.merchant_payment_id')
-        ->join('tracking_statuses as trs','trs.id','dp.status_id')
+        ->join('tracking_statuses as trs','trs.id','p.status_id')
         ->selectRaw('p.payer,p.extra_charge,p.cod,p.price,p.pickup_notes as notes,p.merchant_total,p.receiver_address,p.qr_code,p.status_id,trs.name as status_code,d.id as delivery_id,d.fleet_tracking_number,m.user_name as merchant_name,m.phone as merchant_phone,p.receiver_name,p.receiver_phone,p.delivery_fee,p.taxi_fee,p.remarks,p.id as package_id,p.product_type,p.driver_total,p.billed_kg,p.failed_datetime,p.delivered_datetime,p.arrive_warehouse_datetime,p.returned_datetime')
         // ->whereIn('dp.status_id',$statusIds)
         ->where(function ($q) use ($userId,$statusIds,$userClass) {
@@ -57,9 +57,9 @@ class ReusableService
         ->orderByRaw('dp.status_id = ? ASC',[6])
         ->orderByRaw('
             CASE
-                WHEN dp.status_id = 9 THEN p.delivered_datetime
-                WHEN dp.status_id = 10 THEN p.failed_datetime
-                WHEN dp.status_id = 19 THEN p.failed_datetime
+                WHEN p.status_id = 9 THEN p.delivered_datetime
+                WHEN p.status_id = 10 THEN p.failed_datetime
+                WHEN p.status_id = 19 THEN p.failed_datetime
                 WHEN p.status_id = 11 THEN p.returned_datetime
             END DESC
         ');
