141f61c467e3e4ea9d7836c1d2b2726fae530bbe
Lylay-09 <pozzlay549@gmail.com>
2025-03-14 12:06:44 +0700

    driver report get only unpaid commission package v4

--- app/Http/Controllers/Mobile/Driver/V1/TransactionController.php
+++ app/Http/Controllers/Mobile/Driver/V1/TransactionController.php
@@ -164,6 +164,7 @@ class TransactionController extends Controller
             $qP->where('delivered_datetime','>=',$delCommDatetime);
             $deliveredCount = $qP->count();
         } else $deliveredCount = 0;
+        \Log::error($delCommDatetime);
 
         $qO = Order::where('is_deleted',0)->where('status_id',5)
         // ->where('driver_disbursement_id',$driverId)
