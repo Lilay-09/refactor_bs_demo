1f394a6eed61fb631263aab6ea5ee68d4cc9df42
Lylay-09 <pozzlay549@gmail.com>
2025-03-21 11:48:14 +0700

    check commission from start v3

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -677,11 +677,11 @@ class UserService
         $deliveryRate = $comm->normal_delivery_commission;
         $totalDelivered = $delPkg->delivered_count;
         $totalPickUp = 0;//$pkp->total_package;
-        Log::error($deliveryCommStartDate);
-        Log::error($deliveryRate);
-        Log::error($totalDelivered);
+        // Log::error($deliveryCommStartDate);
+        // Log::error($deliveryRate);
+        // Log::error($totalDelivered);
         $total = Helper::getNumber($pickupRate * $totalPickUp + $deliveryRate * $totalDelivered,2);
-        Log::error($total);
+        // Log::error($total);
         return $total > 0;
     }
 
