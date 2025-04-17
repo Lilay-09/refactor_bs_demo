9558a474dfc1018882e6d119f3b1c4c5edefc152
Lylay-09 <pozzlay549@gmail.com>
2025-04-17 12:38:44 +0700

    notification mobile check date

--- app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
+++ app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
@@ -707,7 +707,15 @@ class HomeScreenController extends Controller
         $user = UserService::getAuthUser('driver');
         $notifications = Notification::where('user_id',$user->id)->where('is_read',0)->orderByDesc('sent_datetime')->selectRaw('id,is_read,title,body,sent_datetime')->get();
         $groupedPackages = collect($notifications)->map(function ($item) {
-            $item->groupKey = date('d-M-Y',strtotime($item->sent_datetime));
+            $sentAt = Carbon::parse($item->sent_datetime);
+            if ($sentAt->isToday()) {
+                $item->groupKey = 'Today';
+            } elseif ($sentAt->isYesterday()) {
+                $item->groupKey = 'Yesterday';
+            } else {
+                $item->groupKey = $sentAt->format('d-M-Y'); // e.g., 17-Apr-2025
+            }
+            $item->groupKey = $sentAt;
             $item->time = Helper::formatCustomDateTime($item->sent_datetime,'h:i A');
             return $item;
         })
