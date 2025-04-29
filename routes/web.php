1a4a4f7ed5a3fab86aaffdc79da27f611680adae
Lylay-09 <pozzlay549@gmail.com>
2025-04-29 10:30:34 +0700

    config cache

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ Route::middleware(['web'])->group(function () {
         return response()->json(['message' => 'Log file not found.'], 404);
     });
 
+    Route::get('/clear-configCache', function () {
+        Artisan::call('config:clear');
+
+        return response()->json(['message' => 'Config cache cleared.']);
+    });
+
     Route::get('/pdf', function(){
         return view('pdf.package_history');
     });
