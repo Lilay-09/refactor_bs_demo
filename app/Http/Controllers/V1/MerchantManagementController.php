92f942fb64562563e791bbc5c8bd57ee12ee4c95
Lylay-09 <pozzlay549@gmail.com>
2025-04-30 14:47:12 +0700

    merchant shops v1

--- app/Http/Controllers/V1/MerchantManagementController.php
+++ app/Http/Controllers/V1/MerchantManagementController.php
@@ -110,7 +110,7 @@ class MerchantManagementController extends Controller
         $id = $req->id;
         $merchant = User::where('is_deleted',0)->where('company_id',$user->company_id)
         ->where('account_type',$this->userClass)
-        ->with(['bank_accounts:id,bank_name,bank_number,account_name,user_id,is_primary','shops:id,owner_id,name_en as shop_name_en,name_km as shop_name_km,phone,est_pcs'])
+        ->with(['bank_accounts:id,bank_name,bank_number,account_name,user_id,is_primary','shops:id,owner_id,name_en as shop_name_en,name_km as shop_name_km,phone,est_pcs,address'])
         ->selectRaw('id,cod_fee,code,name_km,user_name,email,gender,photo_file_name,business_type,phone,client_type_id,address,referrer_uid,cod,pin_address,login_name')
         ->find($id);
         $priceList = DB::table('price_list_names as n')
@@ -124,8 +124,6 @@ class MerchantManagementController extends Controller
             $cod = $merchant->cod;
         }
         $merchant->cod = $cod ? 1:0;
-        $merchant->shop_info = UserShop::where('owner_id',$id)
-        ->select('name_en as shop_name_en','name_km as shop_name_km','*')->first();
         unset($m->merchantType,$m->bank_accounts);
         return ApiResponse::JsonResult($merchant,__('messages.get one'));
     }
