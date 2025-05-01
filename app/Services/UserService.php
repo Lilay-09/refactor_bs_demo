b9b076e2a7dbb514fce559bf1e615bfd9dd45672
Lylay-09 <pozzlay549@gmail.com>
2025-05-01 12:08:45 +0700

    fix user service v3

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -312,7 +312,7 @@ class UserService
 
                     // Save the shop using your service
                     $saveShop = $userShopService->saveShop($shopReq, $user);
-                    if($saveShop->error) return ApiResponse::ValidateFail($saveShop->message);
+                    if($saveShop->error) return ApiResponse::flex($saveShop);
                 }
 
                 // foreach($req->shops as $shop){
