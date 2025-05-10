3909c8123db047fa407009d21982a9d8180bab10
Lylay-09 <pozzlay549@gmail.com>
2025-05-10 11:01:55 +0700

    receiver address or zone name

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Helper;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -94,6 +95,14 @@ class Package extends Model
     {
         return $this->formatDatetime($value);
     }
+
+    public function receiverAddress(): Attribute
+    {
+        return Attribute::get(
+            fn ($value) => $value ?? $this->zone_name
+        );
+    }
+
 
     // public function getArriveWarehouseDatetimeAttribute($value)
     // {
