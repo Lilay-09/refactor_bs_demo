ae47603b9adb982069281216fbce35073a368754
Lylay-09 <pozzlay549@gmail.com>
2025-05-12 15:54:40 +0700

    get assigned zone list v1

--- routes/admin/v1.php
+++ routes/admin/v1.php
@@ -283,7 +283,7 @@ Route::middleware(['jwt','localize','userAccess:admin'])->prefix('admin/v1/{lang
         Route::put('/{id}/toParent',[ZoneController::class,'setToParent']);
 
         Route::put('/assign/driver',[ZoneController::class,'assignZoneToDriver']);
-        Route::get('{id}/assign/subZones',[ZoneController::class,'getAssignSubZones']);
+        Route::get('{id}/assigned/subZones',[ZoneController::class,'getAssignSubZones']);
     });
 
     Route::prefix('priceList')->group(function(){
