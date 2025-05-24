b91784b30266cc9676c003715ff84731270d1799
Lylay-09 <pozzlay549@gmail.com>
2025-05-24 16:14:15 +0700

    merchant summmay V8

--- app/Http/Controllers/V1/ReportController.php
+++ app/Http/Controllers/V1/ReportController.php
@@ -1289,7 +1289,7 @@ class ReportController extends Controller
             // Log::info('L1');
         }
 
-        $statuses = [6, 9, 11, 19];
+        $statuses = [6, 9,10, 11, 19];
         foreach($lastOrder as $p){
             $pkgInfo['5.1']['count'] += 1;
             $pkgInfo['5.1']['total'] += $p->cod ? $p->price:0;//- $p->merchant_total;
