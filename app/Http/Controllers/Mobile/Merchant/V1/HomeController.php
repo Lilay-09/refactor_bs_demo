98d3d68c2d06012f13fd537c4d4de859271a2287
Lylay-09 <pozzlay549@gmail.com>
2025-05-30 16:02:52 +0700

    merchant profile

--- app/Http/Controllers/Mobile/Merchant/V1/HomeController.php
+++ app/Http/Controllers/Mobile/Merchant/V1/HomeController.php
@@ -120,7 +120,7 @@ class HomeController extends Controller
 
     public function getBankAccount(){
         $user = UserService::getAuthUser('merchant');
-        $userBanks = UserBank::where('user_id',$user->id)->selectRaw('id,bank_name,bank_number,account_name,is_primary')->orderByDesc('is_primary')->first();
+        $userBanks = UserBank::where('user_id',$user->id)->selectRaw('id,bank_id,bank_name,bank_number,account_name,is_primary')->orderByDesc('is_primary')->first();
         return ApiResponse::JsonResult($userBanks);
     }
 
