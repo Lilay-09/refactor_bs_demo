8944daccdcc64fc5bd677e851af1219f3c80adc5
Lylay-09 <pozzlay549@gmail.com>
2025-07-24 16:33:08 +0700

    update profile phone 3 lines V1

--- app/Models/City.php
+++ app/Models/City.php
@@ -10,9 +10,9 @@ class City extends Model
     use HasFactory;
     protected $table = 'cities';
     protected $fillable = [
-        'name',
+        'name_en',
         'id',
-        'name_kh',
+        'name_km',
         'country_id',
         'create_uid',
         'update_uid',
