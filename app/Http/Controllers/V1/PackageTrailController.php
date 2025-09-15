d2beea3a23f61d2666b3df8e05015dc359e8f462
Lylay-09 <pozzlay549@gmail.com>
2025-09-15 15:58:52 +0700

    prioritize on at warehouse v3

--- app/Http/Controllers/V1/PackageTrailController.php
+++ app/Http/Controllers/V1/PackageTrailController.php
@@ -59,7 +59,7 @@ class PackageTrailController extends Controller
         })
         ->orderByRaw("
             CASE
-                WHEN status_id = 5 THEN 0
+                WHEN status_id = 5 THEN 0       -- top priority
                 ELSE 1
             END DESC
         ")
@@ -71,11 +71,12 @@ class PackageTrailController extends Controller
         ")
         ->orderByRaw("
             CASE
-                WHEN status_id = 9 THEN delivered_datetime
                 WHEN status_id IN (10, 19) THEN failed_datetime
                 ELSE NULL
             END DESC
-        "); // tertiary
+        ");
+
+
 
         // ->selectRaw('merchant_id,order_id,id,taxi_fee,delivery_type,qr_code,price,driver_id,product_type,dim_z,dim_x,dim_y,status_id,failed_datetime,failure_notes,payer,cod,delivery_fee,receiver_address,zone_code,zone_name,receiver_name,receiver_phone,delivered_datetime,assign_driver_datetime,arrive_warehouse_datetime,driver_total,merchant_total,billed_kg,actual_kg,created_at')
         // ->orderBy('arrive_warehouse_datetime','desc')
