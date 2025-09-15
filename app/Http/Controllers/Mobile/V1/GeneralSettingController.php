01f9d3a6e723bce6e218e74388a41881e3f12d30
Lylay-09 <pozzlay549@gmail.com>
2025-09-15 17:20:52 +0700

    scan add 300s and frontend 60s v3

--- app/Http/Controllers/Mobile/V1/GeneralSettingController.php
+++ app/Http/Controllers/Mobile/V1/GeneralSettingController.php
@@ -485,11 +485,12 @@ class GeneralSettingController extends Controller
         $selfTopic = GeneralSettingService::getGeneralTopics($user->company_id,'driver',$user->id)->private;
         Log::info("Cache key: ".$selfTopic);
         $cache = Cache::get($selfTopic);
+        Log::info("Cache data: ".json_encode($cache));
         if(!$cache) return ApiResponse::NotFound("Request not found or has expired");
         $requester = $cache?->requester;
         // return $cache;
         $requester_id = $cache?->requester_id;
-        Log::info("Cache data: ".json_encode($cache));
+
 
         Cache::forget($selfTopic);
 
