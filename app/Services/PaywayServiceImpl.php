95fb370cc3d1934139c31716f5851fefdf2c692f
Lylay <lylay@Lylays-MacBook-Pro.local>
2025-10-07 12:18:10 +0700

    Driver Receive with method v4

--- app/Services/PaywayServiceImpl.php
+++ app/Services/PaywayServiceImpl.php
@@ -329,7 +329,7 @@ class PaywayServiceImpl implements PaywayService
         $callback = $this->callbackUrl($req);
         $validInputs = $callback['inputs'];
         $decodedJson = $callback['return_params'];
-        Log::info('callback => '.json_encode($req));
+        Log::info('callback => '.json_encode($decodedJson));
         try{
             DB::beginTransaction();
             $updatePkg = $this->updatePackage($decodedJson['package_id'],$validInputs['sub'],$validInputs['sub_type'],$decodedJson);
