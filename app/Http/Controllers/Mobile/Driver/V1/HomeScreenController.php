75c62dc4390b147aa4866e5006278c9d6685e3e7
Lylay <lylay@Lylays-MacBook-Pro.local>
2025-11-05 19:09:27 +0700

    optimize(Driver Delivery Packages): V2

--- app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
+++ app/Http/Controllers/Mobile/Driver/V1/HomeScreenController.php
@@ -580,7 +580,7 @@ class HomeScreenController extends Controller
         $statusId = $req->query('status_id');
         $startDate = $req->query('startDate');
         $endDate = $req->query('endDate');
-        Log::info($req->all());
+        Log::error(json_encode($req->all(),JSON_PRETTY_PRINT));
 
         // Date range filter (only applies when both start & end are given)
         $dateRangeFilter = function ($q) use ($startDate, $endDate, $cutoff) {
