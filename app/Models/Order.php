1be2c822651731c8f159759cb568d0ec4b7a7e03
Lylay <lylay@Lylays-MacBook-Pro.local>
2025-11-13 19:13:50 +0700

    feat(commission type): percentage v1

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\LogsActivity;
+// use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
