964378c3f15f4a57c1a45e04b011496b187e48c5
Lylay <lylay@Lylays-MacBook-Pro.local>
2026-01-02 11:45:24 +0700

    Merchant App: Tracking at warehouse v4

--- app/Http/Controllers/Mobile/Merchant/V1/HomeController.php
+++ app/Http/Controllers/Mobile/Merchant/V1/HomeController.php
@@ -394,6 +394,7 @@ class HomeController extends Controller
                 driver_phone: $q->driver->phone,
                 taxi_fee: ($q->taxi_fee > 0 && $q->payer == 'sender') ? Helper::currencyAmount($q->taxi_fee,'USD'):'$0',
                 fees: $feesFmt,
+                remarks: '',
                 other_fee: $q->other_fee,
                 delivery_fee: $q->delivery_fee,
                 driver_contacts: $driver_contacts
